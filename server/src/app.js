3bb9e28e0432ba05232f1a0b44ca7a3a5073eb82
mabelpertot <mabelgem77@gmail.com>
2026-09-24 20:32:32 -0300

    Reconstruccion de tablas con force true

--- server/src/app.js
+++ server/src/app.js
@@ -25,7 +25,7 @@ app.use('/api/ventas', ventasRoutes);
 const PORT = process.env.PORT || 3000;
 console.log('🔄 Sincronizando base de datos con MySQL...');
 
-sequelize.sync({ alter: true })
+sequelize.sync({ force: true })
     .then(() => {
         console.log('✅ Base de datos MySQL conectada y sincronizada a la perfección.');
 
