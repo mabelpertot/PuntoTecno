db9270a29c26160d8b619119b57a7fb3984253c7
mabelpertot <mabelgem77@gmail.com>
2026-09-24 19:26:22 -0300

    Configurada conexion dinamica a MySQL en db.js

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,9 +1,15 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('punto_tecno_db', 'root', 'root', {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: false 
-});
+const sequelize = new Sequelize(
+    process.env.MYSQL_DATABASE || 'punto_tecno_db',
+    process.env.MYSQL_USER || 'utxryo00nwgymbbe',
+    process.env.MYSQL_PASSWORD || 'R6Tf6Kv7PtZxCsYRt4cu',
+    {
+        host: process.env.MYSQL_HOST || 'bglu9kf7ahuif8jt9myu-mysql.services.clever-cloud.com',
+        port: process.env.MYSQL_PORT || 3306,
+        dialect: 'mysql',
+        logging: false
+    }
+);
 
 module.exports = sequelize;
